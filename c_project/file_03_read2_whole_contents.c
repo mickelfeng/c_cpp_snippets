e66bbcb64f49350c09f701fa3e274461e5f6072a
wgf4242 <wgf4242@gmail.com>
2021-05-21 08:30:32 +0800

    sync

---
+++ c_project/file_03_read2_whole_contents.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[], char **env) {
+    char *buffer = 0;
+    long length;
+    const char *filename = "C:\\program.txt";
+    FILE *f = fopen(filename, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        // ftell 用于得到文件位置指针当前位置相对于文件首的偏移字节数。
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        buffer = malloc(length);
+        if (buffer) {
+            fread(buffer, 1, length, f);
+        }
+        fclose(f);
+    }
+
+    if (buffer) {
+        // start to process your data / extract strings here...
+        printf("%s", buffer);
+        FILE *fptr = fopen("C:\\pg.txt", "w");
+        // fprintf(fptr, "%s", &(buffer[strlen(buffer) - 5])); // last 5 character
+        // fprintf(fptr, "%.5s", buffer); // first 5 character
+    }
+}
