7892822ffba31cf9a2b405fd4c833fb0a6917fe3
wgf4242 <wgf4242@gmail.com>
2022-04-16 19:06:42 +0800

    sync

---
+++ c_project/01_printf.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(void)
+{
+    int a = 0x31;
+    printf("int %d\n", a);
+    printf("hex %x\n", a);
+    printf("hex unsign %X\n", a);
+    printf("char %c\n", a);
+    return 0;
+}
