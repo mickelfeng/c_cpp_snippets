78976bf2f7e12b048d4f19a288071831c56ccfa3
wgf4242 <wgf4242@gmail.com>
2023-09-08 18:27:26 +0800

    sync

--- c_project/01_printf.c
+++ c_project/01_printf.c
@@ -1,6 +1,25 @@
 # include <stdio.h>
 
+void test_printf();
+
+void test_snprintf();
+
 int main(void) {
+    // test_printf();
+    test_snprintf();
+    return 0;
+}
+
+void test_snprintf() {
+    char str[10] = {0};
+    int nLen = snprintf(str, sizeof(str), "123456789012345678");
+    printf("str=%s\n", str);
+    printf("nLen=%d\n", nLen);
+    // str=123456789
+    // nLen=18
+}
+
+void test_printf() {
     int a = 0x31;
     int b = 0xb;
     printf("int %d\n", a);
@@ -8,5 +27,4 @@ int main(void) {
     printf("hex unsign %X\n", a);
     printf("char %c\n", a);
     printf(" %.2x", b); // 0b
-    return 0;
 }
